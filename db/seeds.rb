2684c34d66319da5ff31e5204e8bb183a970062a
ashhcashh1120 <aarchibald20@gmail.com>
2019-07-16 10:38:55 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,111 +19,111 @@ rock =Genre.create(name:"Rock")
 
 Song.create(artist:"Meghan Trainor",
   title:"No Excuses",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/pop/No_Excuses.mp3",
+  audioPath:"/songs/pop/No_Excuses.mp3",
   genre:pop)
 
 Song.create(artist:"Shawn Mendes",
   title:"If I Can't Have You",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/pop/If I Can't Have You.mp3",
+  audioPath:"/songs/pop/If I Can't Have You.mp3",
   genre:pop)
 
 Song.create(artist:"Calvin Harris",
   title:"Feels",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/pop/ Feels .mp3",
+  audioPath:"/songs/pop/ Feels .mp3",
   genre:pop)
 
 Song.create(artist:"Sam Smith",
   title:"Promises",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/pop/No_Excuses.mp3",
+  audioPath:"/songs/pop/No_Excuses.mp3",
   genre:pop)
 
 Song.create(artist:"Ariana Grande",
-  title:"7 Rings", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/randb/7Rings.mp3",
+  title:"7 Rings", audioPath:"/songs/randb/7Rings.mp3",
   genre:randb)
 
 Song.create(artist:"Ty Dollar Sign",
-  title:"Light", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/randb/Lighter.mp3",
+  title:"Light", audioPath:"/songs/randb/Lighter.mp3",
   genre:randb)
 
   Song.create(artist:"SZA",
-  title:"The Weekend", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/randb/The Weekend.mp3",
+  title:"The Weekend", audioPath:"/songs/randb/The Weekend.mp3",
   genre:randb)  
 
 Song.create(artist:"Rihanna", 
-title:"Wild Thoughts", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/randb/wild thoughts.mp3", 
+title:"Wild Thoughts", audioPath:"/songs/randb/wild thoughts.mp3", 
 genre:randb)
 
 Song.create(artist:"Jay-Z",
-title:"The Story of OJ", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rap/The Story of OJ.mp3", 
+title:"The Story of OJ", audioPath:"/songs/rap/The Story of OJ.mp3", 
 genre:rap)
 
 Song.create(artist:"21 Savage",
-  title:"Alot", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rap/a_lot.mp3",
+  title:"Alot", audioPath:"/songs/rap/a_lot.mp3",
    genre:rap)
 
 Song.create(artist:"Nipsey Hussle",
-   title:"Grinding All My Life", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rap/grinding_all_my_life.mp3", 
+   title:"Grinding All My Life", audioPath:"/songs/rap/grinding_all_my_life.mp3", 
    genre:rap)
 
 Song.create(artist:"Drake",
-   title:"Nice for What", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rap/Drake - Nice For What.mp3", 
+   title:"Nice for What", audioPath:"/songs/rap/Drake - Nice For What.mp3", 
    genre:rap)
 
 Song.create(artist:"Disturbed",
-  title:"Are Yor Ready", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rock/Disturbed - Are You Ready Lyrics.mp3",
+  title:"Are Yor Ready", audioPath:"/songs/rock/Disturbed - Are You Ready Lyrics.mp3",
   genre: rock)
 
 
 Song.create(artist:"Weezer",
   title:"Can't knock the hustle",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rock/Weezer - Can't Knock the Hustle (Audio).mp3",
+  audioPath:"/songs/rock/Weezer - Can't Knock the Hustle (Audio).mp3",
   genre: rock)
 
 Song.create(artist:"Godsmack",
-  title:"Bulletproof", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rock/Godsmack - Bulletproof (Official Audio).mp3",
+  title:"Bulletproof", audioPath:"/songs/rock/Godsmack - Bulletproof (Official Audio).mp3",
   genre: rock)
 
 Song.create(artist:"Seether",
-  title:"Betray And Degrade", audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/rock/Betray And Degrade.mp3",
+  title:"Betray And Degrade", audioPath:"/songs/rock/Betray And Degrade.mp3",
   genre: rock)
 
 
 Song.create(artist:"Marshemello",
   title: "Happier",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/alternative/Marshmello ft. Bastille - Happier (Official Lyric Video).mp3",
+  audioPath:"/songs/alternative/Marshmello ft. Bastille - Happier (Official Lyric Video).mp3",
    genre: alternative)
 
 Song.create(artist:"Banks",
    title: "Gimme",
-   audioPath:"/Users/ashleyrchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/alternative/BANKS - Gimme (Official Audio).mp3",
+   audioPath:"/songs/alternative/BANKS - Gimme (Official Audio).mp3",
     genre: alternative)
 
 Song.create(artist:"Black Eyed Peas",
     title: "LoHi",
-    audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/alternative/The Black Keys - LoHi [Official Audio].mp3",
+    audioPath:"/songs/alternative/The Black Keys - LoHi [Official Audio].mp3",
     genre: alternative)
 
     
 Song.create(artist:"Billie Eilish",
   title: "Bury a friend",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/alternative/Billie Eilish - bury a friend.mp3",
+  audioPath:"/songs/alternative/Billie Eilish - bury a friend.mp3",
   genre: alternative)
 
 Song.create(artist:"Chris Stapleton",
   title:"Broken Halos",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/country/Chris Stapleton - Broken Halos (Audio).mp3",
+  audioPath:"/songs/country/Chris Stapleton - Broken Halos (Audio).mp3",
   genre: country)
 Song.create(artist:"Lil Nas X",
   title: "Old Town Road",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/country/Lil Nas X, Billy Ray Cyrus - Old Town Road (Lyrics) Remix.mp3",
+  audioPath:"/songs/country/Lil Nas X, Billy Ray Cyrus - Old Town Road (Lyrics) Remix.mp3",
   genre: country)
 
 Song.create(artist:"Chris Stapleton",
   title: "Broken Halos",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/country/Chris Stapleton - Broken Halos (Audio).mp3",
+  audioPath:"/songs/country/Chris Stapleton - Broken Halos (Audio).mp3",
   genre: country)
 
 Song.create(artist:"Musgraves",
   title: "Slow Burn",
-  audioPath:"/Users/ashleyarchibald/Desktop/Music_Trivia/guess_the_song_backend/songs/country/Kacey Musgraves - Slow Burn.mp3",
+  audioPath:"/songs/country/Kacey Musgraves - Slow Burn.mp3",
   genre: country)
